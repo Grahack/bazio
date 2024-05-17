2cea8003decbcfeeabb2fb5c9431ae195423f838
Christophe Gragnic <cgragnic@netc.fr>
2024-05-17 19:32:39 +0200

    JS: hash detection for the tabs

--- static/tabs.js
+++ static/tabs.js
@@ -2,6 +2,7 @@
 
 const tabElements = document.querySelectorAll('button[role="tab"]');
 const panelElements = document.querySelectorAll('[role="tabpanel"]');
+const hashes = ["io", "blockly", "code", "doc-fr", "doc-en"]
 let activeIndex = 0;
 
 // Listen to clicks on tabs
@@ -28,4 +29,31 @@ function setActivePanel(index) {
 
   // Show the new active panel
   panelElements[index].removeAttribute("hidden");
+
+  // tweak the hash
+  parent.location.hash = hashes[index];
 }
+
+// This is me adding 'hash detection'
+let initialIndex = 0;
+const hash = window.location.hash;
+switch (hash.slice(1)) {
+    case 'io':
+        initialIndex = 0;
+        break;
+    case 'blockly':
+        initialIndex = 1;
+        break;
+    case 'code':
+        initialIndex = 2;
+        break;
+    case 'doc-fr':
+        initialIndex = 3;
+        break;
+    case 'doc-en':
+        initialIndex = 4;
+        break;
+}
+
+setActiveTab(initialIndex);
+// end of hash detection
